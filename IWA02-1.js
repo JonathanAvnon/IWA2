9ad277f0786ff887b04de7734771550b692e56bb
JonathanAvnon <info@josephavnon.com>
2023-08-01 12:23:58 +0200

    IWA02 Challenges 1,2 and 3

---
+++ IWA02-1.js
@@ -0,0 +1,11 @@
+// Challenge 1 
+
+// This is the firstname of the user
+
+const user = 'John';
+
+//This is the lastname of hte user
+
+const surName = 'Smith';
+
+console.log(user, surName)
---
+++ IWA02-2.js
@@ -0,0 +1,4 @@
+// This is the date that a user created their account 
+const date = '10/07/2014'
+
+console.log(date)
